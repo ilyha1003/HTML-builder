4a64b2a1268cc6bc2e2a55985c31af3825eec838
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-01-20 00:27:54 +0300

    feat: solve the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+console.log('Enter text here:');
+
+function filling(str) {
+    writeStream.write(`${str}\n`);
+}
+
+function handling(input) {
+    if (input === 'exit') {
+        writeStream.end();
+        process.exit();
+    } else {
+        filling(input);
+    }
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', handling);
+
+process.on('exit', () => {
+    console.log('See u next time!');
+})
