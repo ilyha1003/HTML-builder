bd5319e58c9eb27c33abc9487e8a9e43b4439fd8
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-01-19 23:49:00 +0300

    feat: solve the task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const read = fs.createReadStream(filePath, 'utf-8');
+
+read.on('data', data => console.log(data));
