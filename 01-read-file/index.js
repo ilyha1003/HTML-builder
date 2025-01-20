d41fb06730bd4f9a62d15602b2154f81431c75ec
ilyha1003 <ilyhasankovich@gmail.com>
2025-01-20 15:57:13 +0300

    fix: fix eslint issues

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,4 +3,4 @@ const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const read = fs.createReadStream(filePath, 'utf-8');
 
-read.on('data', data => console.log(data));
+read.on('data', (data) => console.log(data));
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,25 +7,25 @@ const writeStream = fs.createWriteStream(filePath);
 console.log('Enter text here:');
 
 function filling(str) {
-    writeStream.write(`${str}\n`);
+  writeStream.write(`${str}\n`);
 }
 
 function handling(input) {
-    if (input === 'exit') {
-        writeStream.end();
-        process.exit();
-    } else {
-        filling(input);
-    }
+  if (input === 'exit') {
+    writeStream.end();
+    process.exit();
+  } else {
+    filling(input);
+  }
 }
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.on('line', handling);
 
 process.on('exit', () => {
-    console.log('See u next time!');
-})
+  console.log('See u next time!');
+});
